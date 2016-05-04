3143019f3c42e58a56130249104f057977dd0cdd
Karsten Panier <kpanier@aysada.org>
2016-05-04 10:23:49 +0200

    TE-347 using selections

--- web/src/main/java/org/testeditor/fixture/web/WebDriverFixture.java
+++ web/src/main/java/org/testeditor/fixture/web/WebDriverFixture.java
@@ -8,20 +8,29 @@
  * Contributors:
  * Signal Iduna Corporation - initial API and implementation
  * akquinet AG
+ * itemis AG
  *******************************************************************************/
 package org.testeditor.fixture.web;
 
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testeditor.fixture.core.interaction.FixtureMethod;
 
 public class WebDriverFixture {
 
 	private WebDriver driver;
+	private Logger logger = LogManager.getLogger(WebDriverFixture.class);
 
 	@FixtureMethod
 	public void waitSeconds(long timeToWait) throws InterruptedException {
@@ -32,7 +41,16 @@ public class WebDriverFixture {
 	public void startBrowser(String browser) {
 		switch (browser) {
 		case "firefox":
-			driver = new FirefoxDriver();
+			// if (System.getProperty("os.name").startsWith("Windows")) {
+			// System.setProperty("webdriver.ie.driver",
+			// "c:/vagrant_bin/IEDriverServer.exe");
+			// driver = new InternetExplorerDriver();
+			// } else {
+			File profFile = new File(System.getenv("java.io.tmpdir"), "selenium");
+			profFile.mkdir();
+			logger.debug("Creating firefox profile in: {}", profFile);
+			driver = new FirefoxDriver(new FirefoxProfile(profFile));
+			// }
 			break;
 		case "ie":
 			driver = new InternetExplorerDriver();
@@ -40,6 +58,7 @@ public class WebDriverFixture {
 		case "chrome":
 			driver = new ChromeDriver();
 		}
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	}
 
 	@FixtureMethod
@@ -54,20 +73,59 @@ public class WebDriverFixture {
 
 	@FixtureMethod
 	public void pressEnterOn(String elementLocator) {
-		WebElement element = driver.findElement(By.name(elementLocator));
+		WebElement element = getWebElement(elementLocator);
 		element.submit();
 	}
 
 	@FixtureMethod
 	public void typeInto(String elementLocator, String value) {
-		WebElement element = driver.findElement(By.name(elementLocator));
+		WebElement element = getWebElement(elementLocator);
 		element.sendKeys(value);
 	}
 
 	@FixtureMethod
 	public void clear(String elementLocator) {
-		WebElement element = driver.findElement(By.name(elementLocator));
+		WebElement element = getWebElement(elementLocator);
 		element.clear();
+	}
+
+	@FixtureMethod
+	public void clickOn(String elementLocator) {
+		WebElement element = getWebElement(elementLocator);
+		element.click();
+	}
+
+	@FixtureMethod
+	public String readValue(String elementLocator) {
+		WebElement element = getWebElement(elementLocator);
+		return element.getText();
+	}
+
+	@FixtureMethod
+	public void selectElementInSlection(String elementLocator, String value) {
+		Select select = new Select(getWebElement(elementLocator));
+		select.selectByValue(value);
+	}
+
+	protected WebElement getWebElement(String elementLocator) {
+		logger.info("Lookup element {}", elementLocator);
+		WebElement result = null;
+		if (elementLocator.startsWith("[xpath]")) {
+			result = driver.findElement(By.xpath(elementLocator.substring(elementLocator.indexOf(']') + 1)));
+		}
+		if (elementLocator.startsWith("[name]")) {
+			result = driver.findElement(By.name(elementLocator.substring(elementLocator.indexOf(']') + 1)));
+		}
+		if (elementLocator.startsWith("[link]")) {
+			result = driver.findElement(By.linkText(elementLocator.substring(elementLocator.indexOf(']') + 1)));
+		}
+		if (elementLocator.startsWith("[id]")) {
+			result = driver.findElement(By.id(elementLocator.substring(elementLocator.indexOf(']') + 1)));
+		}
+		if (result == null) {
+			result = driver.findElement(By.name(elementLocator));
+		}
+		return result;
 	}
 
 }
