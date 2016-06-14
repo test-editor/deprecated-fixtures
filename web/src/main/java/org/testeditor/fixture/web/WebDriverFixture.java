6d6d308bb8c205a5487a632997bfbd8b9ceaca59
Karsten Panier <kpanier@aysada.org>
2016-06-14 17:08:15 +0200

    Improve Webfixture
    - using Marionette Driver
    - Using WebDriverManager to handle downloads.

--- web/src/main/java/org/testeditor/fixture/web/WebDriverFixture.java
+++ web/src/main/java/org/testeditor/fixture/web/WebDriverFixture.java
@@ -12,24 +12,35 @@
  *******************************************************************************/
 package org.testeditor.fixture.web;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.MarionetteDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testeditor.fixture.core.interaction.FixtureMethod;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.InternetExplorerDriverManager;
+import io.github.bonigarcia.wdm.MarionetteDriverManager;
 
 public class WebDriverFixture {
 
 	private WebDriver driver;
 	private Logger logger = LogManager.getLogger(WebDriverFixture.class);
+	private String exeuteScript = null;
 
 	@FixtureMethod
 	public void waitSeconds(long timeToWait) throws InterruptedException {
@@ -41,28 +52,76 @@ public class WebDriverFixture {
 	}
 
 	@FixtureMethod
-	public void startBrowser(String browser) {
+	public void setDriver(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	@FixtureMethod
+	public void setExecuteScript(String exec) {
+		exeuteScript = exec;
+	}
+
+	@FixtureMethod
+	public WebDriver startBrowser(String browser) {
 		logger.info("Starting brwoser: {}", browser);
 		switch (browser) {
 		case "default":
 			if (System.getProperty("os.name").startsWith("Windows")) {
-				System.setProperty("webdriver.ie.driver", "c:/vagrant_bin/IEDriverServer.exe");
-				driver = new InternetExplorerDriver();
+				launchIE();
 			} else {
-				launchFirefox();
+				launchFirefoxMarionetteDriver();
 			}
 			break;
 		case "firefox":
 			launchFirefox();
 			break;
+		case "modernfirefox":
+			launchFirefoxMarionetteDriver();
+			break;
 		case "ie":
-			driver = new InternetExplorerDriver();
+			launchIE();
 			break;
 		case "chrome":
-			driver = new ChromeDriver();
+			launchChrome();
 			break;
 		}
 		configureDriver();
+		return driver;
+	}
+
+	@FixtureMethod
+	public WebDriver startFireFoxPortable(String browserPath) {
+		FirefoxBinary binary = new FirefoxBinary(new File(browserPath));
+		FirefoxProfile profile = getFireFoxProfile();
+		driver = new FirefoxDriver(binary, profile);
+		configureDriver();
+		return driver;
+	}
+
+	private void launchIE() {
+		InternetExplorerDriverManager.getInstance().setup();
+		driver = new InternetExplorerDriver();
+	}
+
+	private void launchChrome() {
+		ChromeDriverManager.getInstance().setup();
+		driver = new ChromeDriver();
+		registerShutdownHook(driver);
+	}
+
+	private void launchFirefoxMarionetteDriver() {
+		MarionetteDriverManager.getInstance().setup("v0.7.1");
+		driver = new MarionetteDriver();
+		registerShutdownHook(driver);
+	}
+
+	private void registerShutdownHook(final WebDriver driver) {
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				driver.quit();
+			}
+		});
 	}
 
 	protected void configureDriver() {
@@ -70,10 +129,22 @@ public class WebDriverFixture {
 	}
 
 	private void launchFirefox() {
+		FirefoxProfile profile = getFireFoxProfile();
+		driver = new FirefoxDriver();// profile);
+	}
+
+	private FirefoxProfile getFireFoxProfile() {
 		File profFile = new File(System.getenv("java.io.tmpdir"), "selenium");
+		if (profFile.exists()) {
+			profFile.delete();
+		}
 		profFile.mkdir();
 		logger.debug("Creating firefox profile in: {}", profFile);
 		FirefoxProfile profile = new FirefoxProfile(profFile);
+		profile.setAcceptUntrustedCertificates(true);
+		profile.setAssumeUntrustedCertificateIssuer(false);
+		profile.setPreference("security.mixed_content.block_active_content", false);
+		profile.setPreference("security.mixed_content.block_display_content", true);
 		if (System.getProperty("http.proxyHost") != null) {
 			logger.info("Setting up proxy: {}", System.getProperty("http.proxyHost"));
 			profile.setPreference("network.proxy.type", 1);
@@ -81,7 +152,7 @@ public class WebDriverFixture {
 			profile.setPreference("network.proxy.http_port", System.getProperty("http.proxyPort"));
 			profile.setPreference("network.proxy.no_proxies_on", System.getProperty("http.nonProxyHosts"));
 		}
-		driver = new FirefoxDriver(profile);
+		return profile;
 	}
 
 	@FixtureMethod
@@ -125,6 +196,9 @@ public class WebDriverFixture {
 	}
 
 	protected WebElement getWebElement(String elementLocator) {
+		if (exeuteScript != null) {
+			executeScript();
+		}
 		logger.info("Lookup element {}", elementLocator);
 		WebElement result = null;
 		if (elementLocator.startsWith("[xpath]")) {
@@ -143,6 +217,23 @@ public class WebDriverFixture {
 			result = driver.findElement(By.name(elementLocator));
 		}
 		return result;
+	}
+
+	private void executeScript() {
+		try {
+			System.err.println(exeuteScript);
+			BufferedReader br = new BufferedReader(new FileReader(exeuteScript));
+			StringBuilder sb = new StringBuilder();
+			while (br.ready()) {
+				sb.append(br.readLine()).append("\n");
+			}
+			br.close();
+			logger.info("execute script {}", exeuteScript);
+			((JavascriptExecutor) driver).executeScript(sb.toString());
+		} catch (IOException e) {
+			logger.error("Can't read java script", e);
+			throw new RuntimeException(e);
+		}
 	}
 
 	protected String extractLocatorStringFrom(String elementLocator) {
